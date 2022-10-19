85eb65c439aa14053cc92c900a59defb3e612ae9
IvanKorneev <ivan.kornieiev@gmail.com>
2022-10-19 12:43:34 +0300

    add component search

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,38 @@
+import styles from './Search.module.scss';
+import {ReactComponent as SearchIcon} from "assets/icon-search.svg";
+import React, {useRef} from "react";
+
+interface SearchProps {
+    hasError: boolean,
+    onSubmit: (text: string) => void
+}
+
+export const Search = ({hasError, onSubmit}: SearchProps) => {
+    const searchRef = useRef<HTMLInputElement | null>(null);
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault()
+        const text = searchRef.current?.value || '';
+        if (text) {
+            onSubmit(text)
+            if (searchRef.current) {
+                searchRef.current.value = '';
+            }
+        }
+
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className={styles.search}>
+                <label htmlFor='search' className={styles.label}>
+                    <SearchIcon/>
+                </label>
+                <input type='text' ref={searchRef} className={styles.textField} id='search' name='username'
+                       placeholder='Search GitHub username ...'/>
+
+                {hasError && (<div className={styles.error}> No result</div>)}
+                <button>Search</button>
+            </div>
+        </form>
+    );
+};
