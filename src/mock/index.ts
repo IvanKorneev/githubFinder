dd47de5bd97ec5b91ed012ca07a2b5c3b6bc2d8a
IvanKorneev <ivan.kornieiev@gmail.com>
2022-10-19 13:05:30 +0300

    create mock file

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import {GithubUser} from "../types";
+
+export const defaultUser:GithubUser= {
+    "login": "michey85",
+    "id": 36237995,
+    "avatar_url": "https://avatars.githubusercontent.com/u/36237995?v=4",
+    "name": "Mikhail Nepomniashchii",
+    "company": "EPAM",
+    "blog": "mishanep.com",
+    "bio": "React/Vue developer",
+    "twitter_username": "pcgramota",
+    "public_repos": 60,
+    "followers": 211,
+    "following": 6,
+    "created_at": "2018-02-07T16:18:45Z",
+}
