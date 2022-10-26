4afdbb474c918eceb14170446039e7909591384b
IvanKorneev <ivan.kornieiev@gmail.com>
2022-10-26 11:31:39 +0300

    add function  fetUser

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import {TheHeader} from "./components/TheHeader";
 import {Search} from "./components/Search";
 import {defaultUser} from './mock/index'
 import {UserCard} from "./components/UserCard";
+import {useState} from "react";
+import {GithubError, GithubUser, LocalGithubUser} from "./types";
+import {isGithubUser} from "./utils/typeguards";
 
+const BASE_URL = 'https://api.github.com/users/'
 
 function App() {
+    const [user, setUser] = useState<LocalGithubUser | null>(defaultUser);
+    const fetchUser = async (username: string) => {
+        const url = BASE_URL + username;
+        const res = await fetch(url)
+        const user = await res.json() as GithubUser | GithubError;
+
+        if (isGithubUser(user)) {
+
+        } else {
+            setUser(null)
+        }
+    };
     return (
         <Container>
             <TheHeader/>
-            <Search hasError onSubmit={() => {
-            }}/>
-            <UserCard{...defaultUser}/>
+            <Search hasError={!user} onSubmit={fetchUser}/>
+            {user && (<UserCard{...defaultUser}/>)}
         </Container>
     );
 }
